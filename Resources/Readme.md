8866fdd84a20f70e0271b7378bdd9a828b6b3f8a
Shubham Goyal <tango2890@gmail.com>
2016-04-28 16:45:21 -0700

    Updating Resources Project with Readme

---
+++ Resources/Readme.md
@@ -0,0 +1,1 @@
+#To learn how to change data via JAVA code in layout.
--- Resources/app/src/main/java/edu/scu/sgoyal/resources/MainActivity.java
+++ Resources/app/src/main/java/edu/scu/sgoyal/resources/MainActivity.java
@@ -2,6 +2,9 @@ package edu.scu.sgoyal.resources;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Changing Pic from Cat to Lion
+        ImageView iv = new ImageView(this);
+        iv.setImageResource(R.drawable.lion);
+
+        RelativeLayout.LayoutParams param = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        param.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+        param.addRule(RelativeLayout.CENTER_VERTICAL);
+        iv.setLayoutParams(param);
+
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativeLayout);
+        rl.addView(iv);
+
+
+
+
     }
 }
